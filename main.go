d6931fd0fcdbfd06ded641c59aef53d8f96f783b
fengyaming <fym@maikunet.com>
2020-12-02 23:08:24 +0800

    add grpc错误转http错误

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
@@ -10,7 +11,9 @@ import (
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"log"
 	"net/http"
 	"strings"
@@ -18,6 +21,11 @@ import (
 
 // HTTP and GRPC
 var port string
+
+type httpError struct {
+	Code    int32  `json:"code,omitempty"`
+	Message string `json:"message,omitempty"`
+}
 
 func init() {
 	flag.StringVar(&port, "port", "8001", "server listen port")
@@ -59,6 +67,8 @@ func runHttpServer() *http.ServeMux {
 
 func runGrpcGatewayServer() *runtime.ServeMux {
 	endpoint := "0.0.0.0:" + port
+	// 使用自定义的错误处理方法
+	runtime.HTTPError = grpcGatewayError
 	gwmux := runtime.NewServeMux()
 	dopts := []grpc.DialOption{grpc.WithInsecure()}
 	_ = pb.RegisterTagServiceHandlerFromEndpoint(context.Background(), gwmux, endpoint, dopts)
@@ -75,4 +85,27 @@ func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Ha
 			otherHandler.ServeHTTP(w, r)
 		}
 	}), &http2.Server{})
+}
+
+// grpcGatewayError 将 grpc 错误转换为 json 格式的错误，返回给客户端
+func grpcGatewayError(ctx context.Context, _ *runtime.ServeMux, marshaler runtime.Marshaler,
+	w http.ResponseWriter, _ *http.Request, err error) {
+	s, ok := status.FromError(err)
+	if !ok {
+		s = status.New(codes.Unknown, err.Error())
+	}
+
+	httpError := httpError{Code: int32(s.Code()), Message: s.Message()}
+	details := s.Details()
+	for _, detail := range details {
+		if v, ok := detail.(*pb.Error); ok {
+			httpError.Code = v.Code
+			httpError.Message = v.Message
+		}
+	}
+
+	resp, _ := json.Marshal(httpError)
+	w.Header().Set("Content-Type", marshaler.ContentType())
+	w.WriteHeader(runtime.HTTPStatusFromCode(s.Code())) // 将grpc错误码转换成http的状态码
+	_, _ = w.Write(resp)
 }
